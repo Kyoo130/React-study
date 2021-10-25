b52d8198d00001fbdb6e32b8e25e0e96b2c52647
Kyoo130 <kml9003@kakao.com>
2021-10-25 23:24:36 +0900

    [update] 로그인 기능 commit

--- card-maker/src/app.jsx
+++ card-maker/src/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './app.css';
+import styles from './app.module.css';
+import Login from './components/login/login';
 
-function App() {
+function App({ authService }) {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className={styles.app}>
+      <Login authService={authService} />
     </div>
   );
 }
