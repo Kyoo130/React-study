f6eb371fa9ccbf42e836da83f7c49ff098507d72
Kyoo130 <kml9003@kakao.com>
2022-01-06 23:56:32 +0900

    :sparkles: TodoList Page 생성

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,11 +1,13 @@
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
       <TodoTemplate>
         <TodoInsert />
+        <TodoList />
       </TodoTemplate>
     </div>
   );
