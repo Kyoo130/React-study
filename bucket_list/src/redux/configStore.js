8992205bcbfc8531519fab9074a54a8b1ecf9514
Kyoo130 <kml9003@kakao.com>
2022-01-06 20:56:35 +0900

    Repositories reset commit

--- bucket_list/src/redux/configStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import bucket from "./modules/bucket";
-
-const rootReducer = combineReducers({ bucket });
-
-const store = createStore(rootReducer);
-
-export default store;
