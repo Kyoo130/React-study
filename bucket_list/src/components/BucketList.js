8962c4acb3ee625a8dc047e7e23737e02d641430
Kyoo130 <kml9003@kakao.com>
2022-01-21 23:39:35 +0900

    :truck:	react-router > react-router-dom

--- bucket_list/src/components/BucketList.js
+++ bucket_list/src/components/BucketList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {useNavigate} from "react-router";
+import { useNavigate } from "react-router-dom";
 import {useSelector} from "react-redux";
 
 
