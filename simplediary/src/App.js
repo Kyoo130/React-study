e7f38220f92700c7867bb2f4cc2529d4092a2afb
Kyoo130 <kml9003@kakao.com>
2022-02-09 00:18:42 +0900

    :sparkles: DiaryList Component Add Commit

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEdit from "./components/DiaryEdit";
+import DiaryList from "./components/DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "Kyoo1",
+    content: "Hello React 1",
+    emotion: 5,
+    create_date: new Date().getTime()
+  },
+  {
+    id: 1,
+    author: "Kyoo2",
+    content: "Hello React 2",
+    emotion: 2,
+    create_date: new Date().getTime()
+  },
+  {
+    id: 1,
+    author: "Kyoo3",
+    content: "Hello React 3",
+    emotion: 1,
+    create_date: new Date().getTime()
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <DiaryEdit />
+      <DiaryEdit/>
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
