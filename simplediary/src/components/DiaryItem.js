1273e31770ddb7e371362adb6294a297b05b8e23
Kyoo130 <kml9003@kakao.com>
2022-02-10 16:52:32 +0900

    :sparkles: Data 삭제 기능 생성

--- simplediary/src/components/DiaryItem.js
+++ simplediary/src/components/DiaryItem.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-const DiaryItem = ({author, content, emotion, created_date}) => {
+const DiaryItem = ({onDelete, id, author, content, emotion, created_date}) => {
   return (
     <div className="DiaryItem">
       <div className="info">
-        <span>작성자: {author} | 감정점수: {emotion}</span>
+        <span>작성자: {author} | 감정점수: {emotion} || id: {id}</span>
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
+      <button onClick={()=>{
+        console.log(id)
+        if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+          onDelete(id)
+        }
+      }}>삭제하기</button>
     </div>
   );
 };
